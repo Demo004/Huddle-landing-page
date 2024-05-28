7b9ba941afaf9fb726a9c07f555f9ad3a522fcb9
Ademola Taiwo <taiwoademola465@gmaile.com>
2024-05-28 22:48:58 +0100

    added a few sections

--- src/main.js
+++ src/main.js
@@ -5,6 +5,10 @@ import "../style/style.css";
 import "../style/components/header.css";
 import "../style/components/hero.css";
 import "../style/components/grow.css";
+import "../style/components/flowing.css";
+import "../style/components/users.css";
+import "../style/components/cta.css";
+import "../style/components/footer.css";
 
 // utility
 import "../style/utility.css";
